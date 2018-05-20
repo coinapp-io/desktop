d645636247083759f0982bc819a727563b05fdda
Hunter Long <info@socialeck.com>
2018-05-20 14:20:54 -0700

    attempt to fix travis testing issues

--- .travis/travis-test.sh
+++ .travis/travis-test.sh
@@ -14,7 +14,7 @@ npm install
 
 ganache-cli -d 75.12345 --seed visit exotic maximum -a 10 > /dev/null &
 
-sleep 10
+sleep 30
 
 # 0xe78a0f7e598cc8b0bb87894b0f60dd2a88d6a8ab
 curl -X POST -H 'Content-Type: application/json' -d @test/newcoin.json http://localhost:8545
